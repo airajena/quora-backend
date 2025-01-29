61fc4de5294668db8b8bf391d5c4d08cefe1dd34
Aira Jena <airajena.hack@gmail.com>
2025-01-29 09:46:36 +0530

    feat: Add user repository

---
+++ src/main/java/com/quora/backend/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.quora.backend.repositories;
+
+import com.quora.backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> { }
