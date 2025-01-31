9047c8739ccc98c2df71894b831f5ff6a55feda7
Aira Jena <airajena.hack@gmail.com>
2025-01-31 11:04:54 +0530

    feat: Add tag service

---
+++ src/main/java/com/quora/backend/services/TagService.java
@@ -0,0 +1,34 @@
+package com.quora.backend.services;
+
+import com.quora.backend.dtos.TagDTO;
+import com.quora.backend.models.Tag;
+import com.quora.backend.repositories.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TagService {
+    @Autowired
+    private TagRepository tagRepository;
+
+    public List<Tag> getAllTags() {
+        return tagRepository.findAll();
+    }
+
+    public Optional<Tag> getTagById(Long id) {
+        return tagRepository.findById(id);
+    }
+
+    public Tag createTag(TagDTO tagDTO) {
+        Tag tag = new Tag();
+        tag.setName(tagDTO.getName());
+        return tagRepository.save(tag);
+    }
+
+    public void deleteTag(Long id) {
+        tagRepository.deleteById(id);
+    }
+}
